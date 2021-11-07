cc3eff6915f6e9da7cf6780aa6e05a0ba23cb0c9
Micah Diaz <micah.diaz@du.edu>
2021-11-06 22:33:39 -0600

    fix copy button

--- Assets/script.js
+++ Assets/script.js
@@ -108,7 +108,7 @@ function myFunction() {
   copyText.select();
   copyText.setSelectionRange(0, 99999); /* For mobile devices */
    /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
+  window.navigator.clipboard.writeText(copyText.value);
   if (!navigator.clipboard) {
     console.log("navigator clipboard not available")
     return
